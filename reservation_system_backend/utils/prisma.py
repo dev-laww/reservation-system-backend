11259a6a8aaacae836b0c50222f350154504d97f
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-13 00:51:33 +0800

    chore: moved and updated schema

--- reservation_system_backend/utils/prisma.py
+++ reservation_system_backend/utils/prisma.py
@@ -1,12 +1,14 @@
-import asyncio
-
 from prisma import Prisma
-
 
 prisma = Prisma()
 
 
-async def get_db() -> Prisma:
+async def get_db_session() -> Prisma:
+    """
+    Get session to database.
+
+    :return: new session.
+    """
     if not prisma.is_connected():
         await prisma.connect()
 
